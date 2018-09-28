83985a1cd00cc9888669fc10f68b4fa6950fe06b
Brandan McDevitt <brandan.mcdevitt@gmail.com>
2018-09-28 18:11:45 +0100

    Increase char limit on password hash

--- web/app.py
+++ web/app.py
@@ -22,7 +22,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    hash = db.Column(db.String(80), unique=False, nullable=False)
+    hash = db.Column(db.String(250), unique=False, nullable=False)
 
     def __init__(self, username, email, hash):
         self.username = username
