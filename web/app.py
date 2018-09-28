0f1518c3f748f8e4dc1b0930efc46dc578167b33
Brandan McDevitt <brandan.mcdevitt@gmail.com>
2018-09-28 16:01:52 +0100

    Update required columns, and set initialisers

--- web/app.py
+++ web/app.py
@@ -17,12 +17,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    hash = db.Column(db.String(80), unique=False, nullable=False)
 
-    def __init__(self, username, email, password):
+    def __init__(self, username, email, hash):
         self.username = username
         self.email = email
-        self.password = password
+        self.hash = hash
 
 
     def __repr__(self):
