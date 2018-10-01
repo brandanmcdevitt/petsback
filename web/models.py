e7c435ec2a0cb9a239853cb19a9c0694b204aa12
Brandan McDevitt <brandan.mcdevitt@gmail.com>
2018-10-01 22:50:26 +0100

    Testing database issues with new tables

--- web/models.py
+++ web/models.py
@@ -8,7 +8,6 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     hash = db.Column(db.String(250), unique=False, nullable=False)
-    addresses = relationship("Contact", backref="users")
 
     def __init__(self, username, email, hash):
         self.username = username
@@ -18,8 +17,20 @@ class User(db.Model):
 class Contact(db.Model):
     __tablename__ = "contact"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, unique=True)
+    surname = db.Column(db.String(50))
+    forename = db.Column(db.String(50))
+    address = db.Column(db.String(120))
+    postcode = db.Column(db.String(50))
+    number = db.Column(db.Integer)
+
+    def __init__(self, user_id, surname, forename, address, postcode, number):
+        self.user_id = user_id
+        self.surname = surname
+        self.forename = forename
+        self.address = address
+        self.postcode = postcode
+        self.number = number
 
     # def __repr__(self):
     #     return '<Username %r>' % self.username
