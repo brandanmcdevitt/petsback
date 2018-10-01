bbee2c51de57f4a196ce1c0626b0e7042411c266
Brandan McDevitt <brandan.mcdevitt@gmail.com>
2018-10-01 22:17:39 +0100

    Relational table testing

--- web/models.py
+++ web/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     hash = db.Column(db.String(250), unique=False, nullable=False)
+    addresses = relationship("Contact", backref="users")
 
     def __init__(self, username, email, hash):
         self.username = username
