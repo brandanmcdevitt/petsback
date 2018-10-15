b50601f5638ebaede875bfdad11088f74ee98d2f
Brandan McDevitt <brandan.mcdevitt@gmail.com>
2018-10-15 18:07:10 +0100

    Reduce username length on registration input

--- web/forms.py
+++ web/forms.py
@@ -6,7 +6,7 @@ import datetime
 class RegistrationForm(FlaskForm):
     """Registration form"""
 
-    username = StringField('Username', [validators.Length(min=4, max=25)])
+    username = StringField('Username', [validators.Length(min=4, max=12)])
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
     password = PasswordField('Password', [
         validators.DataRequired(),
