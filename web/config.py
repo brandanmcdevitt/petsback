a32cd9dbd6a591e9791449bfd4afe3fc3df8b078
Brandan McDevitt <brandan.mcdevitt@gmail.com>
2018-11-10 00:18:09 +0000

    Refactor redundant code

--- web/config.py
+++ web/config.py
@@ -9,11 +9,6 @@ S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 DEBUG = True
 PORT = 5000
 
-FIREBASE_API = os.environ.get("FIREBASE_API")
-FIREBASE_AUTH_DOMAIN = os.environ.get("FIREBASE_AUTH_DOMAIN")
-FIREBASE_URL = os.environ.get("FIREBASE_URL")
-FIREBASE_STORAGE_BUCKET = os.environ.get("FIREBASE_STORAGE_BUCKET")
-
 # secret key that persists session data
 KEY = os.environ.get('SECRET_KEY')
 
@@ -22,9 +17,9 @@ ALLOWED_EXTENSIONS = set(['jpg'])
 
 # pyrebase config
 PYREBASE_CONFIG = {
-    "apiKey": FIREBASE_API,
-    "authDomain": FIREBASE_AUTH_DOMAIN,
-    "databaseURL": FIREBASE_URL,
-    "storageBucket": FIREBASE_STORAGE_BUCKET,
+    "apiKey": os.environ.get("FIREBASE_API"),
+    "authDomain": os.environ.get("FIREBASE_AUTH_DOMAIN"),
+    "databaseURL": os.environ.get("FIREBASE_URL"),
+    "storageBucket": os.environ.get("FIREBASE_STORAGE_BUCKET"),
     "serviceAccount": 'firebase.json'
 }
